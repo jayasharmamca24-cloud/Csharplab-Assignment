5aa8a8173be23127fb79ff44af38b69a71bdb44b
jayasharmamca24-cloud <jaya.sharma_mca24@gla.ac.in>
2025-11-05 23:34:38 +0530

    Add files via upload

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,42 +1,78 @@
 ﻿using System;
 
-class Vehicle
+class UserProfile
 {
-    public string Make;
-    public string Model;
-    public int Year;
-}
+    private string username;
+    private string password;
+    private string email;
 
-class Truck : Vehicle
-{
-    public void DisplayDetails()
+    public void SetUsername(string uname)
     {
-        Console.WriteLine($"Truck: {Make}, {Model}, {Year}");
+        username = uname;
+    }
+
+    public string GetUsername()
+    {
+        return username;
+    }
+
+    public void SetPassword(string pwd)
+    {
+        if (pwd.Length >= 6)
+        {
+            password = pwd;
+        }
+        else
+        {
+            Console.WriteLine("Password must be at least 6 characters long.");
+        }
+    }
+
+    public string GetPassword()
+    {
+        return password;
+    }
+
+    public void SetEmail(string mail)
+    {
+        if (mail.Contains("@"))
+        {
+            email = mail;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Email.");
+        }
+    }
+
+    public string GetEmail()
+    {
+        return email;
     }
 }
 
-class Bus : Vehicle
-{
-    public void DisplayDetails()
-    {
-        Console.WriteLine($"Bus: {Make}, {Model}, {Year}");
-    }
-}
-
-class Program2
+class Program1
 {
     static void Main()
     {
-        Truck t = new Truck();
-        t.Make = "Tata";
-        t.Model = "LPT 1613";
-        t.Year = 2020;
-        t.DisplayDetails();
+        UserProfile user1 = new UserProfile();
+        user1.SetUsername("Jaya");
+        user1.SetPassword("12345"); // invalid
+        user1.SetPassword("123456"); // valid
+        user1.SetEmail("jayagmail.com"); // invalid
+        user1.SetEmail("jaya@gmail.com"); // valid
 
-        Bus b = new Bus();
-        b.Make = "Volvo";
-        b.Model = "9400XL";
-        b.Year = 2022;
-        b.DisplayDetails();
+        Console.WriteLine("Username: " + user1.GetUsername());
+        Console.WriteLine("Password: " + user1.GetPassword());
+        Console.WriteLine("Email: " + user1.GetEmail());
+
+        UserProfile user2 = new UserProfile();
+        user2.SetUsername("khushi");
+        user2.SetPassword("abcdef");
+        user2.SetEmail("khushi@example.com");
+
+        Console.WriteLine("\nUsername: " + user2.GetUsername());
+        Console.WriteLine("Password: " + user2.GetPassword());
+        Console.WriteLine("Email: " + user2.GetEmail());
     }
 }
